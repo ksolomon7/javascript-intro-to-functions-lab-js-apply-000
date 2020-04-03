f4d98dca700026d03ea378219142ee59ee299196
modern-fork-2657 <kalkidan.solomon2@gmail.com>
2020-04-03 16:03:24 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,31 @@
+function shout(string){
+  return string.toUpperCase();
+};
+
+function logShout(string){
+  return console.log(string.toUpperCase());
+};
+
+function whisper(string){
+  return string.toLowerCase();
+};
+
+function logWhisper(string){
+  console.log(string.toLowerCase());
+};
+
+function sayHiToGrandma(string){
+
+}
+
+function sayHiToGrandma(string){
+  if(`${string}` === `${string.toUpperCase()}`){
+    return "YES INDEED!"
+  }
+}
+
+function sayHiToGrandma(string){
+  if (`${string}`==="I love you, Grandma."){
+    return "I love you, too."
+  }
+}
